86ae3e0807f01b3d0bccf2eb0c36782dac8c4269
diouf2424 <elimanediouff@gmail.com>
2021-07-15 19:12:13 +0000

    mije en olace de l'entite chambre

---
+++ src/Entity/Chambre.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ChambreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ChambreRepository::class)
+ */
+class Chambre
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $typachambre;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nombrelit;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $typelit;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $statut;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTypachambre(): ?string
+    {
+        return $this->typachambre;
+    }
+
+    public function setTypachambre(string $typachambre): self
+    {
+        $this->typachambre = $typachambre;
+
+        return $this;
+    }
+
+    public function getNombrelit(): ?int
+    {
+        return $this->nombrelit;
+    }
+
+    public function setNombrelit(int $nombrelit): self
+    {
+        $this->nombrelit = $nombrelit;
+
+        return $this;
+    }
+
+    public function getTypelit(): ?string
+    {
+        return $this->typelit;
+    }
+
+    public function setTypelit(string $typelit): self
+    {
+        $this->typelit = $typelit;
+
+        return $this;
+    }
+
+    public function getStatut(): ?bool
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(bool $statut): self
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+}
